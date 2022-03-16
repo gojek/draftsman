a03f6235a5ba8490f098509430e66f087ee7f79d
Jitin Sharma <jitins2892@gmail.com>
2022-03-16 10:00:30 +0530

    Add publish scripts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,13 +5,15 @@ buildscript {
     extra.set("versionCode", 1)
 
     repositories {
+        maven(url = "https://plugins.gradle.org/m2/")
         mavenCentral()
         google()
         jcenter()
     }
     dependencies {
-        classpath ("com.android.tools.build:gradle:4.1.1")
-        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.71")
+        classpath("com.android.tools.build:gradle:4.1.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.71")
+        classpath("io.github.gradle-nexus:publish-plugin:1.1.0")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle.kts files
@@ -26,6 +28,9 @@ allprojects {
     }
 }
 
-tasks.register("clean",Delete::class){
+tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
+
+apply(plugin = "io.github.gradle-nexus.publish-plugin")
+apply(from = "${rootDir}/scripts/publish-root.gradle")
