eff32595750194592edaacd9e2719c4a633e313b
Ragvax <ragvaxofficial@gmail.com>
2022-03-28 18:56:30 +0700

    Add alpha percentage info for text color

--- draftsman/src/main/kotlin/com/gojek/draftsman/internal/utils/ColorUtils.kt
+++ draftsman/src/main/kotlin/com/gojek/draftsman/internal/utils/ColorUtils.kt
@@ -1,5 +1,9 @@
 package com.gojek.draftsman.internal.utils
 
 internal fun getColorHex(color: Int): String {
-    return java.lang.String.format("#%06X", 0xFFFFFF and color)
+    return String.format("#%06X %d%%", 0xFFFFFF and color, getAlphaPercentage(color))
+}
+
+private fun getAlphaPercentage(color: Int): Int {
+    return (color shr 24 and 0xff) * 100 / 255
 }
